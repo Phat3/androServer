c11572adece0676aa6d689f0c844dc37a0851076
Sebastiano Mariani <seba0691@gmail.com>
2014-08-20 13:36:25 +0200

    fine prove inserimento dati a mongodb
    Signed-off-by: Sebastiano Mariani <seba0691@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -26,5 +26,24 @@ router.get('/insertData', function(req, res) {
   res.render('insertData');
 });
 
+router.post('/insert', function(req, res) {
+  // Set our internal DB variable
+    var db = req.db;
+
+    // Set our collection
+    var collection = db.get('performanceCollection');
+
+    // Submit to the DB
+    collection.insert(req.body, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            res.send("benissimo");
+        }
+    });
+});
+
 module.exports = router;
 
